512aefb5c5bfe07b3d8dc3691155d58912632f80
Matix <61623585+matix0@users.noreply.github.com>
2023-09-30 15:14:00 -0300

    modificacoes script selecionar niveis

--- SepseGame/Assets/Scripts/SelecionarNiveisManager.cs
+++ SepseGame/Assets/Scripts/SelecionarNiveisManager.cs
@@ -11,6 +11,8 @@ public class SelecionarNiveisManager : MonoBehaviour
     public GameObject StarCount1, StarCount2;
 
     int starCount;
+    public string nivel;
+    public string caso;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,7 @@ public class SelecionarNiveisManager : MonoBehaviour
         }
         StarCount1.GetComponent<TextMeshProUGUI>().text = "/39";
         StarCount2.GetComponent<TextMeshProUGUI>().text = "/39";
+
     }
 
     // Update is called once per frame
@@ -28,18 +31,55 @@ public class SelecionarNiveisManager : MonoBehaviour
     {
         
     }
+    public void getNivel()
+    {
 
+        nivel = EventSystem.current.currentSelectedGameObject.transform.parent.name;
+       
+        nivel = nivel.Replace(" ", "");
+        Debug.Log(nivel);
+        
+        getCaso();
+
+        string tempNivel = nivel.Remove(0, 5);
+        int numeroNivel = int.Parse(tempNivel);
+
+        //nivel = "caso" + nivel;
+        getCaso();
+
+        int qtdEstrelas;
+
+        qtdEstrelas = PlayerPrefs.GetInt("caso" +numeroNivel.ToString());
+
+        Debug.Log("nivel: " + nivel
+            + "\nqtdEstrelas: " + qtdEstrelas
+            + "\ncaso: " + caso
+            +"\nnumeroNivel: "+numeroNivel);
+        
+    }
+    public void getCaso() {
+        Debug.Log("L57 " + nivel);
+        caso = nivel.Remove(0, 5);
+        caso = "caso"+ nivel;
+        Debug.Log(caso);
+    }
+
+    public void getEstrelasNivel() { 
+    
+    }
     public void selecionarNivel()
     {
+        /*
         string nomePaiBotao = EventSystem.current.currentSelectedGameObject.transform.parent.name;
         nomePaiBotao = nomePaiBotao.Remove(0,5);
         nomePaiBotao =  "Caso"+nomePaiBotao;
         nomePaiBotao = nomePaiBotao.Replace(" ","");
         Debug.Log(nomePaiBotao);
-
         SceneManager.LoadScene(nomePaiBotao);
-    }
+        */
 
+        
+    }
 
     public void irCasosBasicos() {
         camera.transform.position = new Vector3(0,0,-10);
