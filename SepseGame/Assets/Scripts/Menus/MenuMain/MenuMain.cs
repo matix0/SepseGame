faa8143cafa9fb0bcdb0d75060de2b769e3f597d
matix0 <mateusviniciuspw@gmail.com>
2024-02-05 16:43:46 -0300

    add: resetar progresso menu inicial

--- SepseGame/Assets/Scripts/Menus/MenuMain/MenuMain.cs
+++ SepseGame/Assets/Scripts/Menus/MenuMain/MenuMain.cs
@@ -13,7 +13,10 @@ public class MenuMain : MonoBehaviour
     public GameObject anim1, anim2;
     public GameObject contButton;
     public EsteticaNurse EsteticaNurse;
+    public NiveisConcluidos NiveisConcluidos;
     public GameObject slider;
+    public GameObject ModalProgresso;
+    public GameObject ModalProgressoApagado;
 
     private void Start()
     {
@@ -21,13 +24,7 @@ public class MenuMain : MonoBehaviour
         {
             Musica = GameObject.Find("MusicManager").GetComponent<AudioSource>();
             Musica.volume = slider.GetComponent<Slider>().value;
-        }
-        
-        /*for (int i = 0; i < 14; i++)
-        {
-            PlayerPrefs.SetInt("caso" + i.ToString(), 0);
-        }*/
-        
+        }        
     }
 
     public void Play()
@@ -66,5 +63,41 @@ public class MenuMain : MonoBehaviour
     {
         slider = GameObject.Find("VolumeSlider");
         Musica.volume = slider.GetComponent<Slider>().value;
+    }
+
+    public void ModalResetarProgresso() {
+        Debug.Log(ModalProgresso.activeSelf);
+        if (!ModalProgresso.activeSelf)
+        {
+            ModalProgresso.SetActive(true);
+        }
+        else ModalProgresso.SetActive(false);
+
+    }
+    public void HandlerProgressoApagado()
+    {
+        ModalProgresso.SetActive(false);
+        
+        if (!ModalProgressoApagado.activeSelf)
+        {
+            ModalProgressoApagado.SetActive(true);
+        }
+        else {
+            ResetarProgresso();
+            ModalProgressoApagado.SetActive(false);
+        }
+        
+
+    }
+    public void ResetarProgresso() {
+        for (int i = 0; i < 14; i++)
+        {
+            PlayerPrefs.SetInt("caso" + i.ToString(), 0);
+        }
+        for (int i = 0; i < 13; i++)
+        {
+            NiveisConcluidos.casos[i] = false;
+        }
+        Debug.Log("Progresso apagado!!");
     }
 }
